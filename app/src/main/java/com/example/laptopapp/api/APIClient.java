76c261dc9ae8cfe4bb6137db2ba797bf5d4cbb6e
DESKTOP-HM1BUMM\manhx <manhxinhtrinh30@gmail.com>
2022-02-17 20:18:04 +0700

    bo sung api

--- app/src/main/java/com/example/laptopapp/api/APIClient.java
+++ app/src/main/java/com/example/laptopapp/api/APIClient.java
@@ -5,6 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
+    String abv="";
     private static final String BASE_URL = "https://admin-laptop-app.herokuapp.com/";
 
     private static Retrofit retrofit = null;
